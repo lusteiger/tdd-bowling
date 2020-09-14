f8f95dba3c0d77a8f00ec134e7a7c3055c32a8c1
lusteiger <leonardoxy7@gmail.com>
2020-09-14 23:09:33 +0800

    [xinyu lu] implement a function

--- src/main/java/Bowling.java
+++ src/main/java/Bowling.java
@@ -1,9 +1,7 @@
-import java.util.Arrays;
-
 public class Bowling {
 
 
-    public int computing(int[] record) {
+    public int Computing(int[] record) {
         int score = 0;
         if (record.length == 12) {
             for (int i = 0; i < record.length - 2; i++) {
@@ -11,6 +9,43 @@ public class Bowling {
             }
             return score;
         }
+
+        if (record.length > 12 && record.length < 20)
+            return HasStrikePin(record);
+
+        if (record.length == 20)
+            return AllSparePin(record);
+        return 0;
+    }
+
+    public int HasStrikePin(int[] record) {
+        int score = 0;
+        int frame = 0;
+        for (int i = 0; i < record.length; i++) {
+            if (record[i] == 10) {
+                score += record[i] + record[i + 1] + record[i + 2];
+                frame++;
+            } else if (record[i] + record[i + 1] == 10) {
+                score += 10 + record[i + 1];
+                i++;
+                frame++;
+            } else {
+                score += record[i] + record[i + 1];
+                i++;
+                frame++;
+            }
+            if (frame == 10)
+                break;
+        }
+        return score;
+
+
+    }
+
+    public int AllSparePin(int[] record) {
+
+
+
 
         return 0;
     }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,21 +1,26 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
     @Test
-    void should_return_300_when_all_pin() {
-        int[]  record = new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+    void should_return_300_when_all_frames_pin() {
+        int[] record = new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
         int score = 300;
         Bowling bowling = new Bowling();
-        int result = bowling.computing(record);
-        assertEquals(score,result);
+        int result = bowling.Computing(record);
+        assertEquals(score, result);
     }
 
+    @Test
+    void should_return_score_when_has_spare_pin() {
+        int[] record = new int[]{10, 10, 10, 8, 2, 5, 4, 10, 10, 10, 10, 10, 10, 10};
+        int score = 249;
+        Bowling bowling = new Bowling();
+        int result = bowling.Computing(record);
+        assertEquals(score, result);
+    }
 
 
 }
