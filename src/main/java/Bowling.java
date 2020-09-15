8994c4766eaf9e0069a7d74e337391a96c9d9a60
lusteiger <leonardoxy7@gmail.com>
2020-09-15 10:08:35 +0800

    [xinyu lu] refactor the Computing function

--- src/main/java/Bowling.java
+++ src/main/java/Bowling.java
@@ -3,29 +3,10 @@ public class Bowling {
 
     public int Computing(int[] record) {
         int score = 0;
-        
-        if (record.length > 20 || record.length < 12) {
-            throw new IllegalArgumentException();
-        }
-
-        if (record.length == 12) {
-            for (int i = 0; i < record.length - 2; i++) {
-                score += record[i] + record[i + 1] + record[i + 2];
-            }
-            return score;
-        }
-
-        if (record.length > 12 && record.length < 20)
-            return HasStrikePin(record);
-
-        if (record.length == 20)
-            return AllSparePin(record);
-        return 0;
-    }
-
-    public int HasStrikePin(int[] record) {
-        int score = 0;
         int frame = 0;
+
+        if (record.length> 20 || record.length < 11)
+            throw new IllegalArgumentException();
 
         for (int i = 0; i < record.length; i++) {
             if (record[i] == 10) {
@@ -43,18 +24,9 @@ public class Bowling {
             if (frame == 10)
                 break;
         }
-        
+
         return score;
     }
 
-    public int AllSparePin(int[] record) {
-        int score = 0;
 
-        for (int i = 0; i < record.length; ) {
-            score += record[i] + record[i + 1];
-            i = i + 2;
-        }
-        
-        return score;
-    }
 }
